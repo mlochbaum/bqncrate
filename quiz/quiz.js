d3154ed60562f29475dea913e55928f8e00ecef1
Marshall Lochbaum <mwlochbaum@gmail.com>
2020-10-29 13:19:38 -0400

    Fix quiz argument handling since variable conventions have been changed

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -5,12 +5,12 @@ I=_=>{
   um.href="../?"+b.className
   fetch("../table.tsv").then(d=>d.text()).then(d=>{
     ps=d.replace(/[<>&'"]/g,x=>({'<':'&lt;','>':'&gt;','&':'&amp;',"'":'&apos;','"':'&quot;'}[x])).split(/\r?\n/g).splice(1).map(r=>r.split("\t"))
-    .filter(x=>-1===x[0].indexOf("dfns.") && ["Tacit", "Dfn"].includes(x[2]) && ["Monadic Function", "Dyadic Function"].includes(x[3]) && x[4]==="")
+    .filter(x=>["Tacit", "Dfn"].includes(x[2]) && ["Monadic Function", "Dyadic Function"].includes(x[3]) && x[4]==="")
     Q1();Q2();Q3()})}
-E=x=>(["any","a numeric","an integer","a Boolean","a character"]["XMIACYNJBD".indexOf(x[0])%5]+(x[1]?[" scalar"," vector"," matrix"]["svm".indexOf(x[1])]:" array")+(x?" <code>"+x+"</code>":""))
+E=x=>(["any","a numeric","an integer","a Boolean","a character"]["xmiacynjbd".indexOf(x[0])%5]+(x[1]?[" unit"," list"," table"]["012".indexOf(x[1])]:" array")+(x?" <code>"+x+"</code>":""))
 R=x=>x[Math.floor(Math.random()*x.length)]
-X=x=>x[0].match(/^[a-z]*/i)[0]
-Y=x=>x[0].match(/[a-z]*$/i)[0]
+X=x=>x[0].match(/^([a-z][a-z0-9]*)?/i)[0]
+Y=x=>x[0].match(/([a-z][a-z0-9]*)?$/i)[0]
 Q1=_=>{
   c1=R(ps);a=X(c1);w=Y(c1)
   q1.innerHTML=`Write a ${c1[3].match(/\w+/)[0].toLowerCase()} ${c1[5].toLowerCase()} function, <code>${a?a+" ":""}YourFunction ${w}</code>, that for ${a?E(a)+" and ":""}${E(w)}, solves the following ${["easy","medium-difficulty","hard"][(x=>Math.floor(3*x.indexOf(c1)/x.length))(ps.filter(x=>x[2]===c1[2]))]} task:<p>${c1[1]}</p>`
