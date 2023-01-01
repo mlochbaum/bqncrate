ba4d843487ad86fb5b35237e59e3141c4016e122
Marshall Lochbaum <mwlochbaum@gmail.com>
2022-12-31 20:36:56 -0500

    Rewrite quiz variable handling to not depend on functional forms

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -5,26 +5,27 @@ I=_=>{
   um.href="../?"+b.className
   fetch("../table.tsv").then(d=>d.text()).then(d=>{
     ps=d.replace(/[<>&'"]/g,x=>({'<':'&lt;','>':'&gt;','&':'&amp;',"'":'&apos;','"':'&quot;'}[x])).split(/\r?\n/g).slice(1,-1).map(r=>r.split("\t"))
-    .filter(x=>["Tacit", "Dfn"].includes(x[2]) && ["Monadic Function", "Dyadic Function"].includes(x[3]) && x[4]==="")
-    Q1();Q2();Q3()})}
-E=x=>(["any","a numeric","an integer","a Boolean","a character"]["xmiacynjbd".indexOf(x[0])%5]+(x[1]?[" unit"," list"," table"]["012".indexOf(x[1])]:" array")+(x?" <code>"+x+"</code>":""))
+    .filter(x=>!/•/.test(x[0]) && /[xmiacynjbdz]/.test(x[0]))
+    Q1();Q2();Q3()
+  })}
 R=x=>x[Math.floor(Math.random()*x.length)]
-X=x=>x[0].match(/^([a-z][a-z0-9]*)?/i)[0]
-Y=x=>x[0].match(/([a-z][a-z0-9]*)?$/i)[0]
+E=x=>(["any","a numeric","an integer","a Boolean","a character"]["xmiacynjbdz".indexOf(x[0])%5]+(x[1]?[" unit"," list"," table"]["012".indexOf(x[1])]:" array")+(x?" <code>"+x+"</code>":""))
+Z=x=>[...x[0].matchAll(/[xmiacynjbdz][0-2]?/g)].map(m=>m[0])
+L=x=>x.length<=2 ? x.join(" and ") : x.map((s,i)=>i<x.length-1?s:"and "+s).join(", ")
 Q1=_=>{
-  c1=R(ps);a=X(c1);w=Y(c1)
-  q1.innerHTML=`Write a ${c1[3].match(/\w+/)[0].toLowerCase()} ${c1[5].toLowerCase()} function, <code>${a?a+" ":""}YourFunction ${w}</code>, that for ${a?E(a)+" and ":""}${E(w)}, solves the following ${["easy","medium-difficulty","hard"][(x=>Math.floor(3*x.indexOf(c1)/x.length))(ps.filter(x=>x[2]===c1[2]))]} task:<p>${c1[1]}</p>`
+  c1=R(ps);a=Z(c1)
+  q1.innerHTML=`Write an expression that for ${L(a.map(E))}, solves the following ${["easy","medium-difficulty","hard"][(x=>Math.floor(3*x.indexOf(c1)/x.length))(ps.filter(x=>x[2]===c1[2]))]} ${c1[5].toLowerCase()} task:<p>${c1[1]}</p>`
 }
 Q2=_=>{
-  c2=R(ps);a=X(c2);w=Y(c2)
-  q2.innerHTML=`What does the following ${c2[3].match(/\w+/)[0].toLowerCase()} ${c2[5].toLowerCase()} function compute when given ${a?E(a)+" and ":""}${E(w)}?<p><code>${c2[0]}</code></p>`
+  c2=R(ps);a=Z(c2)
+  q2.innerHTML=`What does the following expression compute when given ${L(a.map(E))}? (${c2[5].toLowerCase()})<p><code>${c2[0]}</code></p>`
 }
 Q3=_=>{
-  c3=R(ps);a=X(c3);w=Y(c3)
-  r=/[^\[\]\{\}\(\)\w¯\d&;⋄'#∆⍙⍺⍵⎕⍞ ]/g
+  r=/[^\[\]\{\}\(\)\w¯\d&;⋄' ]/g
+  do{c3=R(ps)}while(!r.test(c3[0]));a=Z(c3)
   n=Math.floor(Math.random()*c3[0].match(r).length)
   i=0;c3d=c3[0].replace(/&lt;|&gt;/g,x=>({'&lt;':'<','&gt;':'>'}[x])).replace(r,x=>i++==n?"@@":x).replace(/<|>/g,x=>({'<':'&lt;','>':'&gt;'}[x])).replace('@@','<del> </del>')
   i=0;c3i=c3[0].replace(/&lt;|&gt;/g,x=>({'&lt;':'<','&gt;':'>'}[x])).replace(r,x=>i++==n?"@←@"+x+"@→@":x).replace(/<|>/g,x=>({'<':'&lt;','>':'&gt;'}[x])).replace('@←@','<ins>').replace('@→@','</ins>')
-  q3.innerHTML=`What is the obscured symbol in the following ${c3[3].match(/\w+/)[0].toLowerCase()} ${c3[5].toLowerCase()} function:<p><code id='p3'>${c3d}</code></p>which when given ${a?E(a)+" and ":""}${E(w)}, solves the task: <p>${c3[1]}</p>`
+  q3.innerHTML=`What is the obscured symbol in the following expression:<p><code id='p3'>${c3d}</code></p>which when given ${L(a.map(E))}, solves the ${c2[5].toLowerCase()} task: <p>${c3[1]}</p>`
 }
 W=x=>{b.className=x?x:"wbg"[1+"wb".indexOf(b.className[0])];um.href="../?"+b.className}
